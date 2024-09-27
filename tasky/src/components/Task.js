100dfc183ad819ed424cfa13ba20ecc9a514723a
unknown <944325009@qq.com>
2024-09-27 14:22:00 +0100

    Completed Tasky Lab 1

---
+++ tasky/src/components/Task.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Task = (props) =>{
+
+    return(
+        <div className="card">
+            <p>{props.title}</p>
+            <p>Due:{props.deadline}</p>
+            <p>{props.children}</p>
+        </div>
+    )
+    
+}
+
+export default Task;
