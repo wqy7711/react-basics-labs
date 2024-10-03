63ab349bfec97233edf6a48c7837f8012fdff05b
unknown <944325009@qq.com>
2024-10-03 10:38:29 +0100

    Added task state

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -6,7 +6,7 @@ const Task = (props) =>{
         <div className="card">
             <p>{props.title}</p>
             <p>Due:{props.deadline}</p>
-            <p>{props.children}</p>
+            <p>{props.description}</p>
         </div>
     )
     
